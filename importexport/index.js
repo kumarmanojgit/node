d221305a4b31a3b93219906610f7a552b18353d6
kumarmanojgit <krmanoj72099@gmail.com>
2024-04-24 18:26:45 +0530

    user define module

---
+++ importexport/index.js
@@ -0,0 +1,12 @@
+// store value 
+//this is  for single method
+// const  kuchhbhi =require("./oper");  
+// console.log(kuchhbhi(55,5));
+
+
+// for multiple method pass method as object
+ const {add,sub,mult, name}=require("./oper");
+ console.log(add(2,3));
+ console.log(sub(10,7));
+ console.log(mult(4,3));
+ console.log(name);
---
+++ importexport/oper.js
@@ -0,0 +1,24 @@
+// this is for single method
+// const add=(a,b) => {
+//   return a+b;
+// };
+// module.exports=add;
+
+
+// for multiple method
+
+const add=(a,b)=>
+{
+  return a+b;
+};
+const sub=(a,b)=>
+{
+ return a-b;
+}
+const mult=(a,b)=>
+{
+  return a*b;
+}
+ const name ="manoj";
+
+ module.exports={add,sub,mult,name};
